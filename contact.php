ce4b4530a9ed2faa54813e7ba37082db0cbd918b
kordldurham <66217662+kordldurham@users.noreply.github.com>
2020-06-14 10:03:02 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -12,4 +12,33 @@ if($_POST["submit"]) {
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
     $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
+?}
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Contact form to email</title>
+</head>
+
+<body>
+
+    <?=$thankYou ?>
+
+    <form method="post" action="contact.php">
+        <label>Name:</label>
+        <input name="sender">
+
+        <label>Email address:</label>
+        <input name="senderEmail">
+
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+
+</body>
+
+</html>
